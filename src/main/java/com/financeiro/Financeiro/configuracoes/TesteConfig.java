230c6c4901c48201c65deddb9273d274761ce1ee
Geovanni dos Santos Correia <geovanni-dsc@hotmail.com>
2022-10-29 00:04:40 -0300

    Alteração paara BD MySql

--- src/main/java/com/financeiro/Financeiro/configuracoes/TesteConfig.java
+++ src/main/java/com/financeiro/Financeiro/configuracoes/TesteConfig.java
@@ -28,16 +28,16 @@ public class TesteConfig implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		Categoria ca1 = new Categoria(null, "Fixas");
-		Categoria ca2 = new Categoria(null, "Variaveis");
-		
-		
-		Conta c1 = new Conta(null, "Equatorial","Energia",Instant.parse("2019-06-20T19:53:07Z"), ca1,ContaStatus.A_PAGAR );
-		Conta c2 = new Conta(null, "BRK","Agua", Instant.parse("2020-07-20T19:53:07Z"),ca2, ContaStatus.PAGO);
-		categoriaRepositorios.saveAll(Arrays.asList(ca1,ca2));
-		contaRepositorios.saveAll(Arrays.asList(c1,c2));
-		
-		
+//		Categoria ca1 = new Categoria(null, "Fixas");
+//		Categoria ca2 = new Categoria(null, "Variaveis");
+//		
+//		
+//		Conta c1 = new Conta(null, "Equatorial","Energia",Instant.parse("2019-06-20T19:53:07Z"), ca1,ContaStatus.A_PAGAR );
+//		Conta c2 = new Conta(null, "BRK","Agua", Instant.parse("2020-07-20T19:53:07Z"),ca2, ContaStatus.PAGO);
+//		categoriaRepositorios.saveAll(Arrays.asList(ca1,ca2));
+//		contaRepositorios.saveAll(Arrays.asList(c1,c2));
+//		
+//		
 	
 	}
 	
